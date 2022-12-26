4c5686f4ca56630aea930a7cba03694abf5d9924
youssefjn <youssefjneyah@gmail.com>
2022-12-26 16:22:52 +0100

    removed unused imports

--- backend/url/src/main/java/com/yj/url/repository/UrlRepository.java
+++ backend/url/src/main/java/com/yj/url/repository/UrlRepository.java
@@ -1,8 +1,6 @@
 package com.yj.url.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
 import com.yj.url.model.Url;
 
 public interface UrlRepository extends JpaRepository<Url, Long> {
